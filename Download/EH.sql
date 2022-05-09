16d6de329076a6a47c1ac9ba84b9da16488dc178
XXJ576665968 <576665968@qq.com>
2022-05-09 15:34:07 +0800

    EH

---
+++ Download/EH.sql
@@ -0,0 +1,105 @@
+/*
+ 搜索&标签
+
+@0
+@1  -MISC
+@2  -全彩
+@3  -COS
+@4  -GIF
+@5  -IMG
+@6  -连载
+00
+MISC
+全彩
+COSPLAY
+IAMGE
+
+ */
+-- MYSQL中批量替换某个字段的部分数据
+UPDATE QUICK_SEARCH
+set name=replace(name, 'uploader', 'COS');
+
+UPDATE FILTER
+set Text=replace(TEXT, 'uploader:', '');
+
+/*
+ 下載&收藏
+ */
+UPDATE DOWNLOADS
+set STATE='3'
+where LABEL not like '%@%'
+   or LABEL is Null;
+
+UPDATE DOWNLOADS
+set STATE='0'
+where LABEL like '%@%';
+
+insert into DOWNLOADS(GID, TOKEN, TITLE, TITLE_JPN, THUMB,
+                      CATEGORY, POSTED, UPLOADER, RATING,
+                      SIMPLE_LANGUAGE, TIME, STATE, LEGACY,
+                      LABEL)
+select GID,
+       TOKEN,
+       TITLE,
+       TITLE_JPN,
+       THUMB,
+       CATEGORY,
+       POSTED,
+       UPLOADER,
+       RATING,
+       SIMPLE_LANGUAGE,
+       TIME,
+       3,
+       0,
+       'MISC'
+from LOCAL_FAVORITES
+where Category = 1;
+
+
+insert into DOWNLOADS(GID, TOKEN, TITLE, TITLE_JPN, THUMB,
+                      CATEGORY, POSTED, UPLOADER, RATING,
+                      SIMPLE_LANGUAGE, TIME, STATE, LEGACY,
+                      LABEL)
+select GID,
+       TOKEN,
+       TITLE,
+       TITLE_JPN,
+       THUMB,
+       CATEGORY,
+       POSTED,
+       UPLOADER,
+       RATING,
+       SIMPLE_LANGUAGE,
+       TIME,
+       3,
+       0,
+       'IMAGESET'
+from LOCAL_FAVORITES
+where Category = 32;
+
+insert into DOWNLOADS(GID, TOKEN, TITLE, TITLE_JPN, THUMB,
+                      CATEGORY, POSTED, UPLOADER, RATING,
+                      SIMPLE_LANGUAGE, TIME, STATE, LEGACY,
+                      LABEL)
+select GID,
+       TOKEN,
+       TITLE,
+       TITLE_JPN,
+       THUMB,
+       CATEGORY,
+       POSTED,
+       UPLOADER,
+       RATING,
+       SIMPLE_LANGUAGE,
+       TIME,
+       3,
+       0,
+       'COSPLAY'
+from LOCAL_FAVORITES
+where Category = 64;
+
+-- DELETE  FROM DOWNLOAD_DIRNAME where GID !=0;
+
+-- Category: Upload( MISC:1 ;IMAGE SET:32 ;COSPLAY:64;)
+
+
